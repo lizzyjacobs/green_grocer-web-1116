9ed44f8ca2aac388ad02c09b875b8f25ad021cf3
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-11-16 21:26:57 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,50 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |food, food_info|
+      if new_hash[food]
+        new_hash[food][:count] += 1
+      else
+        new_hash[food] = food_info
+        new_hash[food][:count] = 1
+      end
+    end
+  end
+new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+coupons.each do |coupon|
+  food = coupon[:item]
+  if cart[food] && cart[food][:count] >= coupon[:num]
+    if cart["#{food} W/COUPON"]
+      cart["#{food} W/COUPON"][:count] += 1
+    else cart["#{food} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+         cart["#{food} W/COUPON"][:clearance] = cart[food][:clearance]
+    end
+    cart[food][:count] = cart[food][:count] - coupon[:num]
+  end
+end
+cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |food, info|
+    if info[:clearance]
+      info[:price] = (info[:price] * 0.8).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  cost = 0
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+  final_cart.each do |food, info|
+    cost += (info[:price] * info[:count]).round(2)
+  end
+  cost = cost * 0.9 if cost > 100
+  cost
 end
